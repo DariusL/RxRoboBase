fb64478fc645a45a4f739fab40e5b3ac04d7644d
Darius Lapūnas <d.lapunas@gmail.com>
2016-05-23 12:12:39 +0300

    Got tests running

--- test/src/androidTest/java/lt/dariusl/rxfirebaseandroid/test/ApplicationTest.java
+++ test/src/androidTest/java/lt/dariusl/rxfirebaseandroid/test/ApplicationTest.java
@@ -1,18 +1,19 @@
-package lt.dariusl.rxfirebaseandroid;
+package lt.dariusl.rxfirebaseandroid.test;
 
-import android.app.Application;
 import android.support.annotation.NonNull;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.ApplicationTestCase;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -20,6 +21,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -27,20 +29,18 @@ import static org.junit.Assert.assertTrue;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ApplicationTest{
+public class ApplicationTest {
+
+    @Before
+    public void setUp() throws Exception {
+        assertThat(
+                GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(InstrumentationRegistry.getTargetContext()),
+                is(ConnectionResult.SUCCESS));
+    }
 
     @Test
     public void testAuthAnonymously() throws Exception {
-        /*final FirebaseOptions options = new FirebaseOptions.Builder()
-                .setApiKey("AIzaSyBfivxlHBchQ4WfEr05T6PYAu21fHjcd-E")
-                .setStorageBucket("rx-firebase.appspot.com")
-                .setApplicationId("lt.dariusl.rxfirebaseandroid")
-                .setDatabaseUrl("https://rx-firebase.firebaseio.com/")
-                .build();
-
-        FirebaseApp.initializeApp(RuntimeEnvironment.application, options);
-        FirebaseDatabase.getInstance().goOnline();*/
+        FirebaseDatabase.getInstance().goOnline();
 
         final CountDownLatch latch = new CountDownLatch(1);
         FirebaseAuth.getInstance()
@@ -54,6 +54,6 @@ public class ApplicationTest{
                 });
 
 
-        assertTrue(latch.await(30, TimeUnit.SECONDS));
+        assertTrue(latch.await(5, TimeUnit.SECONDS));
     }
 }
